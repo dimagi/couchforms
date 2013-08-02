0455f395e443c82d63d54a60c124127a7dea5e4d
Kenn Knowles <kenn.knowles@gmail.com>
2013-08-02 18:26:46 -0400

    Fix up dates and timezone with a tiny test

--- couchforms/tests/test_meta.py
+++ couchforms/tests/test_meta.py
@@ -20,4 +20,20 @@ class TestMeta(TestCase):
         self.assertEqual(date(2010,07,23), xform.metadata.timeEnd.date())
         self.assertEqual("admin", xform.metadata.username)
         self.assertEqual("f7f0c79e-8b79-11df-b7de-005056c00008", xform.metadata.userID)
-        
+
+    def testSurviveMissingDates(self):
+        file_path = os.path.join(os.path.dirname(__file__), "data", "no-dates.xml")
+        xml_data = open(file_path, "rb").read()
+        doc_id, errors = post_authenticated_data(xml_data, 
+                                                 settings.XFORMS_POST_URL, 
+                                                 settings.COUCH_USERNAME,
+                                                 settings.COUCH_PASSWORD)
+        xform = XFormInstance.get(doc_id)
+        xforms_by_xmlns = XFormInstance.view('couchforms/counts_by_type', startkey=[xform.xmlns], endkey=[xform.xmlns, {}], reduce=False)
+
+        vacuous = True
+        for row in xforms_by_xmlns:
+            if row['id'] == xform.get_id:
+                vacuous = False
+                self.assertEqual(row['key'][1:], [1970, 0, 1]) # Javascript months start at zero
+        self.assertFalse(vacuous) # Make sure we did actually test something
